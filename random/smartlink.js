5f1ac55b7f5890a2fc94bf42e6ce6ff0a718459f
aridjayasite <63747936+aridjayasite@users.noreply.github.com>
2021-11-30 23:59:53 +0700

    Create smartlink.js

---
+++ random/smartlink.js
@@ -0,0 +1,12 @@
+var pages = [ 
+
+"https://cqwajn.com/gosl/InNpZCI6MTA3NDQ4Nywic21hcnRsaW5rIjp0cnVlfQ==eyJwaWQiOjEwMjg3Nzcs?si1=download&si2=bitly&utm_source=blogspot&utm_medium=smartlink&utm_campaign=download",
+"https://euizhltcd6ih.com/qphj6zec?key=de18bfa60093deed77fb384a922d2eb3",
+"https://cqwajn.com/gosl/InNpZCI6MTExOTE3OCwic21hcnRsaW5rIjp0cnVlfQ==eyJwaWQiOjEwODY1Mjcs?si1=smartlink&si2=dating",
+"https://jc32arlvqpv8.com/febkqdwq69?key=85936785030b9494c1edaf06c2488cc3",
+
+]; 
+function randomPage() { 
+return pages[Math.round(Math.random() * (pages.length - 1))]; 
+} 
+location.href= randomPage(); 
